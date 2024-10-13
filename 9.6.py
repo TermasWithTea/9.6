1370deef1fc2c3bde745fc4d3de8f7434394fc11
TermasWithTea <167928100+TermasWithTea@users.noreply.github.com>
2024-10-13 14:04:40 +0300

    Update 9.6.py

--- 9.6.py
+++ 9.6.py
@@ -1,18 +1,10 @@
-def func(n):
-    i = 0
-    while i != n:
-        yield i
-        i += 1
-
-obj = func(10)
-for i in obj:
-    print(i)
-
 def all_variants(text):
-    i = 0
-    for i in text:
-        
+    length = len(text)
+    for i in range(length):
+        for j in range(i + 1, length + 1):
+            yield text[i:j]
 
+# Пример использования функции
 a = all_variants("abc")
 for i in a:
     print(i)
