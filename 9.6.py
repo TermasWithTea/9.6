37aef67070b4d1f3d662312e08c551be8971df0d
TermasWithTea <167928100+TermasWithTea@users.noreply.github.com>
2024-10-14 13:36:03 +0300

    Update 9.6.py

--- 9.6.py
+++ 9.6.py
@@ -1,10 +1,12 @@
 def all_variants(text):
     length = len(text)
-    for i in range(length):
-        for j in range(i + 1, length + 1):
-            yield text[i:j]
+    subsequences = []
+    for start in range(length):
+        for end in range(start + 1, length + 1):
+            subsequences.append(text[start:end])
+    return sorted(subsequences, key=lambda x: (len(x), text.index(x[0])))
 
-# Пример использования функции
+# Пример использования
 a = all_variants("abc")
 for i in a:
     print(i)
